bce30e76b8942b3f2083166a2a4e3a67844cefdf
SonIsai09 <102713688+SonIsai09@users.noreply.github.com>
2024-10-14 17:23:02 -0600

    creación del mantenimiento agreegar

--- GamesClub/Guia7/Models/MantenimientoEmpleado.cs
+++ GamesClub/Guia7/Models/MantenimientoEmpleado.cs
@@ -2,6 +2,7 @@
 using Guia7.Models;
 using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.InteropServices;
 
 namespace GamesClub.Models
 {
@@ -11,6 +12,60 @@ namespace GamesClub.Models
         private SqlConnection? conexion;
 
         // Método para listar todos los registros de la tabla "Empleado"
+
+        public int Ingresar(Empleado empleado) {
+
+            try
+            {   
+                //crear objeto de la clase conexión
+                Conexion conex = new();
+                //definir la conexipin a la BD
+                conexion = new(conex.getCadConexion());
+                //abrir la conexión
+                 conexion.Open();
+
+                //Definir variable para almacenar el query
+                SqlCommand comando = new ($"insert into Empleado (codEmpleado, IdTipoEmpleado, Nombres, Apellidos,Dui,Estado,Imagen,Usuario,Clave) values(@codEmpleado,@idTipoEmpleado,@nombres,@apellidos,@dui,@estado,@imagen,@usuario,@clave)",  conexion);
+                comando.Parameters.Add("@codEmpleado", SqlDbType.VarChar);
+                comando.Parameters.Add("@idTipoEmpleado", SqlDbType.VarChar);
+                comando.Parameters.Add("@nombres", SqlDbType.VarChar);
+                comando.Parameters.Add("@apellidos", SqlDbType.VarChar);
+                comando.Parameters.Add("@dui", SqlDbType.VarChar);
+                comando.Parameters.Add("@estado", SqlDbType.VarChar);
+                comando.Parameters.Add("@imagen", SqlDbType.VarChar);
+                comando.Parameters.Add("@usuario", SqlDbType.VarChar);
+                comando.Parameters.Add("@clave", SqlDbType.VarChar);
+
+                //Pasar los datos digitrados pó el usuario a los parametros de la instrición SQL
+
+                comando.Parameters["@codEmpleado"].Value=empleado.codEmpleado;
+                comando.Parameters["@idTipoEmpleado"].Value = empleado.IdTipoEmpleado;
+                comando.Parameters["@nombres"].Value = empleado.Nombres;
+                comando.Parameters["@apellidos"].Value = empleado.Apellidos;
+                comando.Parameters["@dui"].Value = empleado.Dui;
+                comando.Parameters["@estado"].Value = empleado.Estado;
+                comando.Parameters["@imagen"].Value = empleado.Imagen;
+                comando.Parameters["@usuario"].Value = empleado.Usuario;
+                comando.Parameters["@clave"].Value = empleado.Clave;
+
+                // Ejecutar instrucción SQL
+                int ingresado= comando.ExecuteNonQuery();
+                conexion.Close();
+                // Devolvemos el numero de registros ingresados a la base
+                return ingresado;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                // Control de errores
+                string error = ex.Message;
+                return 0;
+            }
+        }
+
+
 
 
         public List<Empleado> ListarTodos() {
